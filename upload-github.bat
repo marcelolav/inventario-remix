2faed5a832f9b91630b1f9c89b84db36990d496e
marcelolav <marcelo.lavandeira@gmail.com>
2023-08-24 21:19:06 -0300

    primer commit

---
+++ upload-github.bat
@@ -0,0 +1,7 @@
+echo "# inventario-remix" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/marcelolav/inventario-remix.git
+git push -u origin main
